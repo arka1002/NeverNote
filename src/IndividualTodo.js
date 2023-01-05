6174fd2dc08bbe06a2a012597fb385f61909f503
arka1002 <arka.mcu700@gmail.com>
2023-01-05 19:29:48 +0530

    done componentizing the indie todos

---
+++ src/IndividualTodo.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { Amplify, API, graphqlOperation } from 'aws-amplify'
+import { createTodo } from './graphql/mutations'
+import { listTodos } from './graphql/queries'
+import { withAuthenticator, Button, Heading, Text, TextField, View } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+
+export default function IndividualTodos({ key, title, content }) {
+    return (
+        <div key={key}>
+            <Heading
+                width='30vw'
+                level={6} 
+            >
+                {title}
+            </Heading>
+            
+            <Text
+                variation="primary"
+                as="p"
+                color="black"
+                lineHeight="1.5em"
+                fontWeight={400}
+                fontSize="1em"
+                fontStyle="normal"
+                textDecoration="none"
+                width="30vw"
+            >
+                {content}
+            </Text>
+        </div>
+    );
+}
