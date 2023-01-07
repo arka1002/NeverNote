a49648957d9cc4301bcfd0d07a4bd9cab007fef8
arka1002 <arka.mcu700@gmail.com>
2023-01-07 22:32:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
   useQueryClient,
 } from '@tanstack/react-query';
 import IndieTodos from './components/IndieTodos';
-import { Flex, View, useTheme } from '@aws-amplify/ui-react';
+import { Flex, View, useTheme, TextField } from '@aws-amplify/ui-react';
 import { useForm } from "react-hook-form";
 
 
@@ -24,13 +24,15 @@ const App = ({ signOut, user }) => {
 
   // Access the client
   const queryClient = useQueryClient();
-  
+
   // Queries
-  const query = useQuery({ queryKey: ['todos'], queryFn: async () => {
-    const todoData = await API.graphql(graphqlOperation(listTodos));
-    const todos = todoData.data.listTodos.items;
-    return todos;
-  } })
+  const query = useQuery({
+    queryKey: ['todos'], queryFn: async () => {
+      const todoData = await API.graphql(graphqlOperation(listTodos));
+      const todos = todoData.data.listTodos.items;
+      return todos;
+    }
+  })
   return (
     <>
       <div style={styles.container}>
@@ -39,18 +41,28 @@ const App = ({ signOut, user }) => {
         <h2>NeverNote</h2>
       </div>
 
-    {/* The input form */}
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="name" {...register("name", { required: true })} />
-      {errors.name && <span>This field is required</span>}
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("description", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.description && <span>This field is required</span>}
-      
-      <input type="submit" />
-    </form>
+      {/* The input form */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <TextField
+          descriptiveText="Enter a valid name"
+          placeholder="Name"
+          label="Name"
+          errorMessage={errors.name} {...register("name", { required: true })}
+        />
+        {errors.name && <span>This field is required</span>}
+        {/* include validation with required or other standard HTML validation rules */}
+        <TextField
+          descriptiveText="Enter a valid description"
+          placeholder="Description"
+          label="Description"
+          errorMessage={errors.name} {...register("description", { required: true })}
+        />
+        {/* errors will return when field validation fails  */}
+        {errors.description && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
 
 
 
@@ -72,8 +84,8 @@ const App = ({ signOut, user }) => {
           backgroundColor={tokens.colors.blue[20]}
         >
           {query.data?.map((todo) => (
-          <IndieTodos id={todo.id} namey={todo.name} description={todo.description} status={todo.status} />
-        ))}
+            <IndieTodos id={todo.id} namey={todo.name} description={todo.description} status={todo.status} />
+          ))}
         </View>
         <View
           height="100%"
@@ -81,8 +93,8 @@ const App = ({ signOut, user }) => {
           backgroundColor={tokens.colors.blue[40]}
         >
           {query.data?.filter(label => label.status == "NOT DONE").map((todo) => (
-          <IndieTodos id={todo.id} namey={todo.name} description={todo.description} status={todo.status} />
-        ))}
+            <IndieTodos id={todo.id} namey={todo.name} description={todo.description} status={todo.status} />
+          ))}
         </View>
         <View
           height="100%"
@@ -90,8 +102,8 @@ const App = ({ signOut, user }) => {
           backgroundColor={tokens.colors.blue[60]}
         >
           {query.data?.filter(label => label.status == "DONE").map((todo) => (
-          <IndieTodos id={todo.id} namey={todo.name} description={todo.description} status={todo.status} />
-        ))}
+            <IndieTodos id={todo.id} namey={todo.name} description={todo.description} status={todo.status} />
+          ))}
         </View>
       </Flex>
     </>
@@ -100,7 +112,7 @@ const App = ({ signOut, user }) => {
 
 const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
+  todo: { marginBottom: 15 },
   input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
   todoName: { fontSize: 20, fontWeight: 'bold' },
   todoDescription: { marginBottom: 0 },
